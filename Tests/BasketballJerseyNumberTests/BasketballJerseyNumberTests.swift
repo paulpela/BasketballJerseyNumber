d3d7b9c2d23737c7596c9fa6ce62e7a8bdc81be6
Paul Pela <paulpela@gmail.com>
2020-10-03 12:49:27 +0200

    Test if FIBAJerseyNumber conforms to BasketballJerseyNumber

--- Tests/BasketballJerseyNumberTests/BasketballJerseyNumberTests.swift
+++ Tests/BasketballJerseyNumberTests/BasketballJerseyNumberTests.swift
@@ -5,16 +5,18 @@ final class BasketballJerseyNumberTests: XCTestCase {
     func testTypesUseProtocol() {
         let jerseyNumbers: [BasketballJerseyNumber?] = [
             NCAAJerseyNumber(number: "0"),
-            PZKoszJerseyNumber(number: "0")
+            PZKoszJerseyNumber(number: "0"),
+            FIBAJerseyNumber(number: "0")
         ]
         
-        XCTAssertEqual(jerseyNumbers.count, 2)
+        XCTAssertEqual(jerseyNumbers.count, 3)
     }
     
     func testCanAccessNumberValue() {
         let jerseyNumbers: [BasketballJerseyNumber?] = [
             NCAAJerseyNumber(number: "0"),
-            PZKoszJerseyNumber(number: "0")
+            PZKoszJerseyNumber(number: "0"),
+            FIBAJerseyNumber(number: "0")
         ]
         
         jerseyNumbers.compactMap({ $0 }).forEach { (jersey) in
